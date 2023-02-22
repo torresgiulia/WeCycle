0c49445e0393d5efd7e63d8a945a6b6c1efe0ad9
Giulia Torres <giulia.nataly@hotmail.com>
2023-02-22 10:37:15 +0000

    teste mudança de utilizador

--- screens/BarcodeScreen.js
+++ screens/BarcodeScreen.js
@@ -1,65 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import ProductScreen from './ProductScreen';
 
-export default function BarcodeScreen({navigation}) {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [id, setId] = useState('Produto não escaneado');
+//minuto: 19
+//Para passar informação de uma página para a outra: navigation.navigate("Página", {valor1 = xxx, valor2= xxx})
+//na Stack.Screen: options={({rota}) => { title: rota.params.valor1, title: rota.params.valor2}}
+//useRoute?? 
 
-  const askForCameraPermission = () => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })()
-  }
+const BarcodeScreen = ({navigation}) => {
 
-  // Request Camera Permission
-  useEffect(() => {
-    askForCameraPermission();
-  }, []);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [scanned, setScanned] = useState(false);
+    const [id, setId] = useState('Produto não escaneado');
+
+    const askForCameraPermission = () => {
+        (async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+        })()
+    }
+
+    // Permissão da camera
+    useEffect(() => {
+        askForCameraPermission();
+    }, []);
 
 
 
-  // What happens when we scan the bar code
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    setId(data)
-    console.log('Type: ' + type + '\nData: ' + data)
-    //Abrir nova página aqui
-    navigation.navigate('Product');
-    //navigation.navigate(ProductScreen)
-  };
+    // Código escaneado
+    const handleBarCodeScanned = ({ type, data }) => {
+        setScanned(true);
+        setId(data)
+        console.log('Type: ' + type + '\nData: ' + data)
+        //Abrir nova página aqui
+        navigation.navigate('Product');
+    };
 
-  // Check permissions and return the screens
-  if (hasPermission === null) {
+    // Checar permissões
+    if (hasPermission === null) {
+        return (
+        <View style={styles.container}>
+            <Text>Requesting for camera permission</Text>
+        </View>)
+    }
+    if (hasPermission === false) {
+        return (
+        <View style={styles.container}>
+            <Text style={{ margin: 10 }}>Sem acesso a camera garantido, por favor aceder as configurações do dispositivo e garantir acesso da camera a aplicação</Text>
+        </View>)
+    }
+
+    // Permissão concedida
     return (
-      <View style={styles.container}>
-        <Text>Requesting for camera permission</Text>
-      </View>)
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={{ margin: 10 }}>Sem acesso a camera garantido, por favor aceder as configurações do dispositivo e garantir acesso da camera a aplicação</Text>
-      </View>)
-  }
+        <View style={styles.container}>
+        <View style={styles.barcodeContainer}>
+            <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={{ height: 400, width: 400 }} />
+        </View>
+        <Text style={styles.maintext}>{id}</Text>
 
-  // Return the View
-  return (
-    <View style={styles.container}>
-      <View style={styles.barcodeContainer}>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={{ height: 400, width: 400 }} />
-      </View>
-      <Text style={styles.maintext}>{id}</Text>
-
-      {scanned && <Button title={'Escanear novamente'} onPress={() => setScanned(false)} color='tomato' />}
-    </View>
-  );
+        {scanned && <Button title={'Escanear novamente'} onPress={() => setScanned(false)} color='tomato' />}
+        </View>
+    );
 }
+export default BarcodeScreen;
 
 const styles = StyleSheet.create({
   container: {
