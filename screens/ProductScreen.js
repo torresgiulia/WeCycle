823a3db8a2f64d700f7e0a70229cb1aceeee3314
Giulia Torres <giulia.nataly@hotmail.com>
2023-03-05 20:38:11 +0000

    tentativa falha, não entra no map

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,13 +1,67 @@
+//NAVIGATION
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//REACT
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { useEffect, useState, Component } from 'react';
+
+//FIREBASE
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function ProductScreen({route, navigation}){
+    //detalhes do produto
     const itemId = route.params;
+    const produto = {
+        nome: '',
+        fabricante: '',
+        instrucoes: '',
+        link: '',
+    }
+
+    //products call
+    const [products, setProducts] = useState([]);
+    const productRef = collection(db, "products");
+
+    useEffect(() => {     
+        getProduct();
+    }, [])
+
+    //Todos os produtos
+    const getProduct = async () => {
+        const data = await getDocs(productRef);
+        setProducts(data.docs.map((doc)=> ({...doc.data(), id: doc.id})));
+        handleProduct();
+        
+    };  
+
+    //Produto lido
+    function handleProduct() {
+        console.log("hadleProduct");
+        products.map((product) =>{
+            console.log("map");
+            // console.log(product.id);
+            // console.log(itemId);
+            if(product.id == itemId.toString()){
+                console.log("entrou");
+                produto.nome= product.nome;
+                produto.fabricante= product.fabricante;
+                produto.instrucoes= product.instrucoes;
+                produto.link= product.link;
+            }
+            else{
+                console.log("nadaa");
+            }
+        })
+    } 
+
+
     return(
         <View style={styles.container}>
             <View>
                 <Text>ID: {JSON.stringify(itemId)}</Text>
+                <View>{products.map((product) => {return(<Text>{product.nome}</Text>)})}</View>
             </View>
             <TouchableOpacity>
                 <Text onPress={() => navigation.navigate('Barcode')}>Voltar</Text>
